ca26b05ef02189851e0dddfcc4c1b698d5ce94a9
Savitha-mani <Savitha.mani@yahoo.com>
2026-02-25 11:18:30 -0700

    second commit

--- src/test/java/org/com/projects/SeleniumProjects/project1/LoginLogout.java
+++ src/test/java/org/com/projects/SeleniumProjects/project1/LoginLogout.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.DataProvider;
@@ -18,8 +19,12 @@ public class LoginLogout {
 	
 	@Test(dataProvider = "getData")
 	public void loginTest(HashMap<String, String> input) {
-		WebDriver driver=new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-popup-blocking");
+		options.setExperimentalOption("excludeSwitches", new String[]{"disable-popup-blocking"});
+		WebDriver driver=new ChromeDriver(options);
 		String email=input.get("email");
+		String password=input.get("password");
 		driver.get("https://www.edureka.co/");
 		driver.manage().window().maximize();
 		WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(15));
@@ -32,7 +37,7 @@ public class LoginLogout {
 		loginEmail.sendKeys(email);
 		WebElement loginPassword=driver.findElement(By.id("loginPassword"));
 		loginPassword.clear();
-		loginPassword.sendKeys("Edu!2345");
+		loginPassword.sendKeys(password);
 		WebElement loginBtn=driver.findElement(By.xpath("//button[text()='LOG IN']"));
 		loginBtn.click();
 		
@@ -44,8 +49,10 @@ public class LoginLogout {
 	public Object[][] getData() {
 		HashMap<Object, Object> map=new HashMap<>();
 		map.put("email", "savitha.mani@yahoo.com");
+		map.put("password","Edu!2345" );
 		HashMap<Object, Object> map1=new HashMap<>();
 		map1.put("email", "savith1a.mani@yahoo.com");
+		map1.put("password","Edu!2345" );
 		return new Object[][] {
 			{map},{map1}
 			};
