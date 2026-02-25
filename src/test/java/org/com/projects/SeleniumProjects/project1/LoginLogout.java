c5e71159fc6021aa774f8d9a2975891b39a1e92d
Savitha-mani <Savitha.mani@yahoo.com>
2026-02-24 20:25:21 -0700

    first commit

---
+++ src/test/java/org/com/projects/SeleniumProjects/project1/LoginLogout.java
@@ -0,0 +1,54 @@
+package org.com.projects.SeleniumProjects.project1;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+import javax.sound.sampled.TargetDataLine;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LoginLogout {
+	
+	@Test(dataProvider = "getData")
+	public void loginTest(HashMap<String, String> input) {
+		WebDriver driver=new ChromeDriver();
+		String email=input.get("email");
+		driver.get("https://www.edureka.co/");
+		driver.manage().window().maximize();
+		WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(15));
+		WebElement login=driver.findElement(By.xpath("//button[text()='Log in']"));
+		login.click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("loginFormEmail")));
+		WebElement loginEmail=driver.findElement(By.id("loginFormEmail"));
+		
+		loginEmail.clear();
+		loginEmail.sendKeys(email);
+		WebElement loginPassword=driver.findElement(By.id("loginPassword"));
+		loginPassword.clear();
+		loginPassword.sendKeys("Edu!2345");
+		WebElement loginBtn=driver.findElement(By.xpath("//button[text()='LOG IN']"));
+		loginBtn.click();
+		
+		
+	}
+	
+	@DataProvider
+	
+	public Object[][] getData() {
+		HashMap<Object, Object> map=new HashMap<>();
+		map.put("email", "savitha.mani@yahoo.com");
+		HashMap<Object, Object> map1=new HashMap<>();
+		map1.put("email", "savith1a.mani@yahoo.com");
+		return new Object[][] {
+			{map},{map1}
+			};
+	}
+
+}
